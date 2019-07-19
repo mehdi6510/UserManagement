37fbae71980390e953c218081637940cba31e526
mehdi6510 <mehdiafifi@gmail.com>
2019-07-20 01:45:40 +0430

    Change user properties

--- backend/src/main/java/com/afifi/usermng/model/User.java
+++ backend/src/main/java/com/afifi/usermng/model/User.java
@@ -11,39 +11,26 @@ public class User {
 
     @Id
     private String id;
-    private String nationalNumber;
     private String name;
     private String lastName;
     private String username;
     private String password;
-    private String address;
-    private String fixedPhone;
     private String cellPhone;
-    private Date birthDate;
-    private Date registrationDate;
-    private int age;
-    private int score;
+    private Date creationDate;
     private char status;
     private char type;
 
     public User() {
     }
 
-    public User(String nationalNumber, String name, String lastName, String username, String password,
-                String address, String fixedPhone, String cellPhone, Date birthDate, Date registrationDate,
-                int age, int score, char status, char type) {
-        this.nationalNumber = nationalNumber;
+    public User(String name, String lastName, String username, String password,
+                String cellPhone, Date creationDate, char status, char type) {
         this.name = name;
         this.lastName = lastName;
         this.username = username;
         this.password = password;
-        this.address = address;
-        this.fixedPhone = fixedPhone;
         this.cellPhone = cellPhone;
-        this.birthDate = birthDate;
-        this.registrationDate = registrationDate;
-        this.age = age;
-        this.score = score;
+        this.creationDate = creationDate;
         this.status = status;
         this.type = type;
     }
@@ -54,14 +41,6 @@ public class User {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getNationalNumber() {
-        return nationalNumber;
-    }
-
-    public void setNationalNumber(String nationalNumber) {
-        this.nationalNumber = nationalNumber;
     }
 
     public String getName() {
@@ -96,22 +75,6 @@ public class User {
         this.password = password;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getFixedPhone() {
-        return fixedPhone;
-    }
-
-    public void setFixedPhone(String fixedPhone) {
-        this.fixedPhone = fixedPhone;
-    }
-
     public String getCellPhone() {
         return cellPhone;
     }
@@ -120,36 +83,12 @@ public class User {
         this.cellPhone = cellPhone;
     }
 
-    public Date getBirthDate() {
-        return birthDate;
+    public Date getCreationDate() {
+        return creationDate;
     }
 
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Date getRegistrationDate() {
-        return registrationDate;
-    }
-
-    public void setRegistrationDate(Date registrationDate) {
-        this.registrationDate = registrationDate;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
     }
 
     public char getStatus() {
@@ -173,49 +112,36 @@ public class User {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return age == user.age &&
-                score == user.score &&
-                status == user.status &&
+        return status == user.status &&
                 type == user.type &&
                 id.equals(user.id) &&
-                nationalNumber.equals(user.nationalNumber) &&
                 name.equals(user.name) &&
                 lastName.equals(user.lastName) &&
                 username.equals(user.username) &&
                 password.equals(user.password) &&
-                Objects.equals(address, user.address) &&
-                Objects.equals(fixedPhone, user.fixedPhone) &&
-                Objects.equals(cellPhone, user.cellPhone) &&
-                Objects.equals(birthDate, user.birthDate) &&
-                registrationDate.equals(user.registrationDate);
+                cellPhone.equals(user.cellPhone) &&
+                creationDate.equals(user.creationDate);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, nationalNumber, name, lastName, username,
-                password, address, fixedPhone, cellPhone, birthDate,
-                registrationDate, age, score, status, type);
+        return Objects.hash(id, name, lastName, username, password, cellPhone, creationDate, status, type);
     }
 
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("User{");
-        sb.append("id=").append(id);
-        sb.append(", nationalNumber='").append(nationalNumber).append('\'');
+        sb.append("id='").append(id).append('\'');
         sb.append(", name='").append(name).append('\'');
         sb.append(", lastName='").append(lastName).append('\'');
         sb.append(", username='").append(username).append('\'');
         sb.append(", password='").append(password).append('\'');
-        sb.append(", address='").append(address).append('\'');
-        sb.append(", fixedPhone='").append(fixedPhone).append('\'');
         sb.append(", cellPhone='").append(cellPhone).append('\'');
-        sb.append(", birthDate=").append(birthDate);
-        sb.append(", registrationDate=").append(registrationDate);
-        sb.append(", age=").append(age);
-        sb.append(", score=").append(score);
+        sb.append(", creationDate=").append(creationDate);
         sb.append(", status=").append(status);
         sb.append(", type=").append(type);
         sb.append('}');
         return sb.toString();
     }
+
 }
