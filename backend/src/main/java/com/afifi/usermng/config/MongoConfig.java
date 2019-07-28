a90c6a0f0eee42f2c22aed7280f7b2b6fdf762a6
Mehdi Afifi <mehdiafifi@gmail.com>
2019-07-28 10:49:04 +0430

    Setting embeded mongo db

--- backend/src/main/java/com/afifi/usermng/config/MongoConfig.java
+++ backend/src/main/java/com/afifi/usermng/config/MongoConfig.java
@@ -1,24 +1,29 @@
 package com.afifi.usermng.config;
 
 import com.mongodb.MongoClient;
+import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.io.IOException;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "com.afifi.usermng.repository")
 @EnableMongoAuditing
-public class MongoConfig extends AbstractMongoConfiguration {
+public class MongoConfig {
 
-    @Override
-    public MongoClient mongoClient() {
-        return new MongoClient("localhost", 27017);
+    private static final String MONGO_DB_URL = "localhost";
+    private static final String MONGO_DB_NAME = "embeded_db";
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws IOException {
+        EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
+        mongo.setBindIp(MONGO_DB_URL);
+        MongoClient mongoClient = mongo.getObject();
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, MONGO_DB_NAME);
+        return mongoTemplate;
     }
-
-    @Override
-    protected String getDatabaseName() {
-        return "user_management";
-    }
-
 }
